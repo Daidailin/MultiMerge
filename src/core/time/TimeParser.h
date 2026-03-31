86245949f852a306b131c1db8e02b18508cf47df
Daidailin <121416107+Daidailin@users.noreply.github.com>
2026-03-31 16:43:43 +0800

    Update TimeParser.h with current implementation

--- src/core/time/TimeParser.h
+++ src/core/time/TimeParser.h
@@ -1,22 +1,16 @@
 #ifndef TIMEPARSER_H
 #define TIMEPARSER_H
 
+#include <QString>
 #include "TimePoint.h"
-#include <string>
 
 class TimeParser {
-private:
-    static const int BUFFER_SIZE = 256;
-
 public:
-    // 解析时间字符串为TimePoint
-    static TimePoint parse(const std::string& timeStr);
-
-    // 尝试多种时间格式解析
-    static bool tryParse(const std::string& timeStr, TimePoint& result);
-
-    // 检查字符串是否为有效的时间格式
-    static bool isValidTimeFormat(const std::string& timeStr);
+    // 解析时间字符串为TimePoint对象
+    static TimePoint parse(const QString& timeStr);
+    
+    // 检查时间字符串是否有效
+    static bool isValidTimeString(const QString& timeStr);
 };
 
 #endif // TIMEPARSER_H
